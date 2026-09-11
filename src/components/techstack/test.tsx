0a091baafba230b4d48e466d2fd8338c14a7dd1f
Tahxin <satahsin6@gmail.com>
2026-09-11 23:41:31 +0600

    removed test.tsx file

--- src/components/techstack/test.tsx
+++
@@ -1,9 +0,0 @@
-export default function mystack() {
-  return (
-    <div className="card bg-base-100 shadow-sm hover:shadow-md border border-base-200">
-        <h1>Your Stack</h1>
-        <p>No Technologies selected yet.</p>
-
-    </div>
-  );
-}
