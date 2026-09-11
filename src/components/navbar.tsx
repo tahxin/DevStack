c4929cdee02fbe2da65f6445fbc0a13cac332589
Tahxin <satahsin6@gmail.com>
2026-09-11 18:57:53 +0600

    second draft of navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,12 +2,12 @@ import Logo from '../assets/logo-text.png';
 
 const navbar = () => {
     return (
-        <nav className="flex column items-center justify-between p-4 bg-gray-800 text-white">
+        <nav className="flex column items-center justify-between p-4">
             <div className="logo">
                 <img src={Logo} alt="Logo" />
             </div>
             <div className="nav-links">
-                <ul>
+                <ul className="flex space-x-4">
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">Technologies</a></li>
                     <li><a href="/projects">Projects</a></li>
@@ -15,9 +15,9 @@ const navbar = () => {
                     <li><a href="/contact">Contact</a></li>
                 </ul>
             </div>
-            <div className="nav-actions">
-                <button className="login-btn">Sign In</button>
-                <button className="signup-btn">Sign Up</button>
+            <div className="nav-actions ">
+                <button className="login-btn btn btn-primary">Sign In</button>
+                <button className="signup-btn btn btn-secondary color-[#D91B7E]">Sign Up</button>
             </div>
         </nav>
     );
